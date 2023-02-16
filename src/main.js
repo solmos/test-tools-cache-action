0a512a2e99f53bad16e4481ea0549727f9429bff
sergio.olmos <sergio.olmos@sanofi.com>
2023-02-16 12:06:00 +0100

    Created initial action js code

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function run() {
+  try {
+    await getNode12();
+  } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
+  }
+}
+
+async function getNode12() {
+  const downloadUrl = 'https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz';
+  // const tempDir = process.env['RUNNER_TEMP'];
+
+  try {
+    console.log('Download node12 tar');
+    const downloadPath = await tc.downloadTool(downloadPath);
+  } catch (error) {
+    throw new Error(`Failed to download Node12: ${error}`);
+  }
+
+  const extractedPath = await tc.extractTar(downloadPath);
+
+  const cachedPath = await tc.cacheDir(extractedPath, 'node', '12.7.0');
+  core.addPath(cachedPath);
+}
+
+run();
