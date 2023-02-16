648ca16f4c7a12021f076fa03bea27095d2953a3
sergio.olmos <sergio.olmos@sanofi.com>
2023-02-16 12:31:48 +0100

    Moved download step out of the try structure

--- src/main.js
+++ src/main.js
@@ -13,13 +13,14 @@ async function getNode12() {
   const downloadUrl = 'https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz';
   // const tempDir = process.env['RUNNER_TEMP'];
 
-  try {
-    console.log('Download node12 tar');
-    const downloadPath = await tc.downloadTool(downloadUrl);
-  } catch (error) {
-    throw new Error(`Failed to download Node12: ${error}`);
-  }
+  // try {
+  //   console.log('Download node12 tar');
+  //   const downloadPath = await tc.downloadTool(downloadUrl);
+  // } catch (error) {
+  //   throw new Error(`Failed to download Node12: ${error}`);
+  // }
 
+  const downloadPath = await tc.downloadTool(downloadUrl);
   const extractedPath = await tc.extractTar(downloadPath);
 
   const cachedPath = await tc.cacheDir(extractedPath, 'node', '12.7.0');
